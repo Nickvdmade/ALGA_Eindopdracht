df55841bac3ab9d742a196954435a250f3310189
Nickvdmade <nickvdmade@gmail.com>
2017-10-09 20:40:31 +0200

    UI toegevoegd

--- Eindopdracht/Eindopdracht/Main.cpp
+++ Eindopdracht/Eindopdracht/Main.cpp
@@ -1,13 +1,47 @@
 #include "Dungeon.h"
 
+void ShowUI(Dungeon* dungeon)
+{
+	system("cls");
+	dungeon->Print();
+	cout << "You have the following choices:" << endl;
+	cout << "- talisman" << endl;
+	cout << "- handgrenade" << endl;
+	cout << "- compass" << endl;
+	cout << "- quit" << endl;
+	cout << endl;
+	cout << "-> ";
+}
+
 void main()
 {
-	Dungeon *dungeon = new Dungeon(4,4);
+	Dungeon* dungeon = new Dungeon(4,4);
 	dungeon->FillDungeon();
 	
-	dungeon->Print();
-	
-	getchar();
+	string choice = "";
+	while (choice != "quit")
+	{
+		ShowUI(dungeon);
+		getline(cin, choice);
+		if (choice == "talisman")
+		{
+			cout << "talisman used" << endl;
+		}
+		if (choice == "handgrenade")
+		{
+			cout << "handgrenade used" << endl;
+		}
+		if (choice == "compass")
+		{
+			cout << "compass used" << endl;
+		}
+		if (choice != "quit")
+		{
+			cout << "press enter to continue";
+			getchar();
+		}
+	}
+
 	delete dungeon;
 
 	_CrtDumpMemoryLeaks();
